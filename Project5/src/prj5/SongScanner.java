1a76ca9e83f1b7abf376f3c8175ac37914b2b6f9
mashlyn6 <mashlyn6@vt.edu>
2017-04-19 14:30:30 -0400

    Update SongScanner.java

--- Project5/src/prj5/SongScanner.java
+++ Project5/src/prj5/SongScanner.java
@@ -2,11 +2,31 @@ package prj5;
 
 public class SongScanner {
 
-    public void scanSongs(String songFile) {
-        // TODO Auto-generated method stub
-        
+    public LinkedSongList<Song> scanSongs(String songFile) {
+        LinkedSongList<Song> songList = new LinkedSongList<Song>();
+        try
+        {
+            Scanner file = new Scanner(new File(songFile));
+            int index = 0;
+            file.nextLine();
+            while (file.hasNextLine())
+            {
+                String[] songArray = file.nextLine().split(" *, *");
+                for (int i = 0; i < songArray.length; i++)
+                {
+                    songList.add(new Song(songArray[i], songArray[i+1]), Integer.valueOf(songArray[i+2], songArray[i+3]), index;
+                    index += 2;
+                }
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+             //should we do something with this?
+        }
+        return songList;
     }
 
+                                 //I dont think we need either of the methods below but I'm not sure
     public SONGLIST getSongList() {
         // TODO Auto-generated method stub
         return null;
