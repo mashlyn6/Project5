59c6f7eb4cf291566cb75b8d18bc47cd4633ae7e
mashlyn6 <mashlyn6@vt.edu>
2017-04-19 10:36:31 -0400

    Update Input.java

--- Project5/src/prj5/Input.java
+++ Project5/src/prj5/Input.java
@@ -1,5 +1,19 @@
 package prj5;
 
+/**
+ * @author 
+ * @version
+ * This class begins the program by creating a PersonScanner and SongScanner 
+ */
 public class Input {
 
+  /**
+   * creates a new PersonScanner and SongScanner using the given file
+   * @param the file that the PersonScanner and SongScanner will use
+   */
+  public static void main(String[] args)
+  {
+      PersonScanner scanner1 = new PersonScanner(args);
+      SongScanner scanner2 = new SongScanner(args);
+  }  
 }
